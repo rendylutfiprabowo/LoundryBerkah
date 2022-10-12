fa52f96d7db83736382cfde47197310dab233d9c
RendyLutfiPrabowo <81194811+rendylutfiprabowo@users.noreply.github.com>
2022-10-12 18:26:24 +0700

    Create tentangkami.php

---
+++ app/Views/menu/tentangkami.php
@@ -0,0 +1,44 @@
+<?= $this->extend('templates/template'); ?>
+<?= $this->section('content'); ?>
+
+<div id="about" class="about-us section " >
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="left-image wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.5s">
+                    <img src="/assets/seo/images/about-left-image.png" alt="">
+                </div>
+            </div>
+            <div class="col-lg-6 align-self-center wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
+                <div class="section-heading">
+                    <h6>Tentang Kami</h6>
+                    <h2>Kami konsisten <em>membantu, membersihkan &amp; meringankan </em> kebutuhan <span>mahasiswa di Kampung Baru</span></h2>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-sm-4">
+                        <div class="about-item">
+                            <h4>500+</h4>
+                            <h6>Mahasiswa terbantu</h6>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-4">
+                        <div class="about-item">
+                            <h4>490+</h4>
+                            <h6>Merasa terjangkau</h6>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-4">
+                        <div class="about-item">
+                            <h4>600+</h4>
+                            <h6>Menjadi langganan kami!</h6>
+                        </div>
+                    </div>
+                </div>
+                <p style="text-align:justify;"><a rel="nofollow" href="/home" target="_parent">Loundry Berkah</a> sebuah usaha rumahan milik kumpulan ibu-ibu warga RT.02 RW.03 Kelurahan Kampung Baru, Kota Bandarlampung. Loundry berkah tidak hanya membantu mahasiswa, namun terus berfokus agar mampu memberikan dampak positif bagi warga sekitar, khususnya mereka yang tidak berpenghasilan seperti ibu rumah tangga sekitar kami. Terima kasih atas kepercayaan anda, bantu kami agar terus konsisten dan meningkatkan pelayanan kami dengan mengirimkan saran atau kritik melalui link berikut.</p>
+                <div class="main-green-button"><a href="#">Kirim <b>Saran & Kritik</b></a></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?= $this->endsection('content'); ?>
